cb697d8fa61417adc0826a237c927d5f3fce73ff
ColinTree <colinycl123@gmail.com>
2017-03-27 01:17:36 +0800

    添加本地文件/add local files

---
+++ ColinTreeSysLang.java
@@ -0,0 +1,51 @@
+/**
+ * @author ColinTree (colinycl123@gmail.com)
+ */
+
+package cn.colintree.aix;
+
+import android.content.*;
+import android.util.*;
+import com.google.appinventor.components.annotations.*;
+import com.google.appinventor.components.common.*;
+import com.google.appinventor.components.runtime.*;
+import com.google.appinventor.components.runtime.util.*;
+
+import java.util.Locale;
+
+@DesignerComponent(version = ColinTreeSysLang.VERSION,
+    description = "by ColinTree at http://aix.colintree.cn",
+    category = ComponentCategory.EXTENSION,
+    nonVisible = true,
+    iconName = "images/extension.png")
+
+@SimpleObject(external = true)
+
+public class ColinTreeSysLang extends AndroidNonvisibleComponent
+implements Component {
+    public static final int VERSION = 1;
+    private ComponentContainer container;
+    private Context context;
+    private static final String LOG_TAG = "ColinTreeSysLang";
+    public ColinTreeSysLang(ComponentContainer container) {
+        super(container.$form());
+        this.container = container;
+        context = (Context) container.$context();
+        Log.d(LOG_TAG, "ColinTreeSysLang Created" );
+    }
+    
+    @SimpleFunction(description = "Get System default language")
+    public String SysLang() {
+    	Locale locale = context.getResources().getConfiguration().locale;
+    	String language = locale.getLanguage();
+    	return language;
+    }
+    @SimpleFunction(description = "Return if system default language is chinese")
+    public boolean SysLangIsZh() {
+    	return (SysLang().endsWith("zh"));
+    }
+    @SimpleFunction(description = "Return if system default language is english")
+    public boolean SysLangIsEn() {
+    	return (SysLang().endsWith("en"));
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+## 获取系统语言
+
+更新于2016.12.2 版本1.0
+
+三个过程，一个是获取系统语言，另外两个分别是判断是否中文或者英文
+
+原网址：http://aix.colintree.cn/down/ColinTreeSysLang
+
+
+
+## Get System Language
+ 
+Version1.0 Update in Dec.2, 2016
+
+Three procedure, get system language code, if system language is Chinese and if syste language is English
+
+Original Web Page：http://aix.colintree.cn/down/ColinTreeSysLang
